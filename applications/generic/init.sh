47b964e0884b55ec585725a78b1903afa518adc8
Nicolas Thumann <nicolas.thumann@greenbone.net>
2024-06-13 12:04:50 +0200

    Change: Fix init.sh for Debian

--- applications/generic/init.sh
+++ applications/generic/init.sh
@@ -3,8 +3,8 @@ set -e
 
 source /etc/os-release
 # In Debian 10 and Ubuntu 20.10 or earlier the init file is called differently
-if [ "$NAME" = "Debian" ] && dpkg --compare-versions "$VERSION_ID" le 10 \
-    || [ "$NAME" = "Ubuntu" ] && dpkg --compare-versions "$VERSION_ID" le 20.10
+if [ "$ID" = "debian" ] && dpkg --compare-versions "$VERSION_ID" le 10 \
+    || [ "$ID" = "ubuntu" ] && dpkg --compare-versions "$VERSION_ID" le 20.10
 then
     service mysql start
 else
